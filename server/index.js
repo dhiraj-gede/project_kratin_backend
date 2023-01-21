15ead2b8fe6e362c4cabd06d5ebf94e0ca25e574
Dhiraj Gede <42700091+dhiraj-gede@users.noreply.github.com>
2023-01-21 18:03:01 +0530

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app  = require('./app');
+// const mongoose = require('mongoose');
+// const config = require('./src/config/config');
+
+// const DB_URI = config.mongoose.url
+// mongoose.set('strictQuery', false);
+
+
+// mongoose.connect(DB_URI) 
+// .then(()=>{
+//     console.log('connection established at ' + DB_URI)
+// })
+// .catch(err=>console.log('connection error--> ', err))
+
+
+
+const server = app;
+// const PORT = config.port
+
+server.listen(8080, ()=>{
+    console.log('listening on port http://localhost:8080');
+}) 
