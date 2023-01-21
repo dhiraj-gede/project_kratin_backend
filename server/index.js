413639804e8cf8e229563f7ede177e725bae257a
Dhiraj Gede <42700091+dhiraj-gede@users.noreply.github.com>
2023-01-21 18:32:17 +0530

    vercel attempt 8

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app  = require('./app');
-// const mongoose = require('mongoose');
-// const config = require('./src/config/config');
+const mongoose = require('mongoose');
+const config = require('./src/config/config');
 
-// const DB_URI = config.mongoose.url
-// mongoose.set('strictQuery', false);
+const DB_URI = config.mongoose.url
+mongoose.set('strictQuery', false);
 
 
-// mongoose.connect(DB_URI) 
-// .then(()=>{
-//     console.log('connection established at ' + DB_URI)
-// })
-// .catch(err=>console.log('connection error--> ', err))
+mongoose.connect(DB_URI) 
+.then(()=>{
+    console.log('connection established at ' + DB_URI)
+})
+.catch(err=>console.log('connection error--> ', err))
 
 
 
 const server = app;
+console.log(config)
 const PORT = process.env.PORT
 server.listen(PORT, ()=>{
     console.log('listening on port http://localhost:'+PORT);
